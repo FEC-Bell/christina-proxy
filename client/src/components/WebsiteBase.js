1089735abe40b3b85ceff37a5efe98fd0ff60366
Christina Wang <cywang117@outlook.com>
2020-07-06 21:20:35 +0900

    Complete responsive header with navbar
    - All links are dummy links
    - Desktop version for width >= 940px
    - Mobile version for width < 940px
    - Mobile version has toggleable side menu, which:
        - Toggles closed when clicking away from it
        - Disables page vertical scroll on toggle open
        - Comes with backdrop that dims background content
    - Mobile & desktop menus come with hover / toggle transitions similar
    to actual Steam page

---
+++ client/src/components/WebsiteBase.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import styled from 'styled-components';
+import { GlobalStyle } from '../GlobalStyle';
+import GlobalHeader from './GlobalHeader';
+
+const TopPadBody = styled.div`
+  padding-top: 104px;
+  z-index: -1;
+  @media screen and (max-width: 940px) {
+    padding-top: 62px;
+  }
+`;
+
+const WebsiteBase = () => {
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <GlobalHeader />
+      <TopPadBody>
+        <div>First Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+        <div>Module</div>
+      </TopPadBody>
+    </React.Fragment>
+  );
+}
+
+export default WebsiteBase;
